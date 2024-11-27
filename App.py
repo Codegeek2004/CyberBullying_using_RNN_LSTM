7cfa0deb6741c7968b6b36d6b188f2d3744e7cf1
Codegeek2004 <singhjanvi2021@gmail.com>
2024-11-27 14:49:27 +0530

    23rd commit

--- App.py
+++ App.py
@@ -1,11 +1,14 @@
+from flask import Flask, render_template
 import webbrowser
-from flask import Flask, render_template, request
-#from text_prediction_rnn import predict_cyberbullying
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    # Render the index.html file
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    # Open the app in the browser automatically
+    # Open the app in the browser automatically when the server starts
     webbrowser.open('http://127.0.0.1:5000/')
     app.run(debug=True)
