1621d71506c16293785126917510591b02baa42e
Codegeek2004 <singhjanvi2021@gmail.com>
2024-11-27 14:46:30 +0530

    22nd Commit

--- App.py
+++ App.py
@@ -1,18 +1,9 @@
 import webbrowser
 from flask import Flask, render_template, request
-from text_prediction_rnn import predict_cyberbullying
+#from text_prediction_rnn import predict_cyberbullying
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html', text_input='', result=None)
-
-@app.route('/predict', methods=['POST'])
-def predict():
-    user_input = request.form['text']
-    result = predict_cyberbullying(user_input)
-    return render_template('index.html', result=result, text_input=user_input)
 
 if __name__ == '__main__':
     # Open the app in the browser automatically
